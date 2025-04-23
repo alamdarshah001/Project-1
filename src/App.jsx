9dfb1772528cad0a21c948b39d49b9d7f55ee972
alamdarshah001 <alamdarbukhari842@gmail.com>
2025-04-23 10:17:53 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from './Components/Navigation'
+import Mein from './Components/Mein'
+import "./App.css"
+function App() {
+
+  return (
+    <div>
+      <Navigation/>
+      <Mein/>
+    </div>
+
+  )
+}
+
+export default App
